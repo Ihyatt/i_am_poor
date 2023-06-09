2c9ebeec9cfe82b5a06c6d1bfeee985b379bc65b
inas hyatt <inashyatt@unknown86cc9c913f13.attlocal.net>
2023-06-09 14:03:56 -0700

    updated yaml, images folder and added new .png to main page

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,14 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amber[200],
+        backgroundColor: Colors.green[600],
         appBar: AppBar(
           title : Text('I am very poor'),
-          backgroundColor: Colors.amber[700],
+          backgroundColor: Colors.teal[300],
         ),
         body: Center(
           child: Image(
-            image: NetworkImage('https://psmag.com/.image/t_share/MTI3NTgxOTc4Mjk0ODA4NTQ2/sad-piggy-bank.jpg'),
+            image: AssetImage('images/empty_wallet.png'),
           )
         ),
       ),
